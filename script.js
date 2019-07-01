c4f5e1cdbbb70c849b2c4a86110d74e62e051ff9
CrazyElfi <al.gaffarova.26@gmail.com>
2019-07-02 00:38:11 +0300

    new commit

--- script.js
+++ script.js
@@ -1,98 +1,72 @@
-
 function newRow(){
+    console.log("newRow");
+    // for (var j=0; j < 10000; j++) {
+    //
+    //     var row = document.createElement("TR");
+    //     row.setAttribute("class", "row");
+    //     row.id = "row_" + j;
+    //
+    //     for (var i = 0; i < 5; i++) {
+    //         var data = document.createElement("TD")
+    //         data.className = "myTd";
+    //         data.innerHTML = "text" + i + j;
+    //         row.appendChild(data);
+    //     }
+    //     document.getElementById("myTable").appendChild(row);
+    // }
+}
+var arr = [1,2,3,4,5,6,7,8,9,10,11];
 
-    for (var j=0; j < 10000; j++) {
+var startIndex = 0;
+var endIndex = 3;
+var range = endIndex - startIndex;
+var canShowNext = true;
+var canShowPrev = true;
 
-        var row = document.createElement("TR");
-        row.setAttribute("class", "row");
-        row.id = "row_" + j;
-
-        for (var i = 0; i < 5; i++) {
-            var data = document.createElement("TD")
-            data.className = "myTd";
-            data.innerHTML = "text" + i + j;
-            row.appendChild(data);
-
+function print(startIndex, endIndex) {
+    var printArr = [];
+    for(var i = startIndex; i < endIndex; i++) {
+        if(arr[i] !== undefined) {
+            newRow();
+            printArr.push(arr[i]);
         }
-        document.getElementById("myTable").appendChild(row);
 
     }
+    if(printArr.length)
+        console.log(printArr);
 }
-console.log('testset');
 
-// console.log(data)
-var arr = [];
-for(var i = 0; i < 10; i++) {
-    arr.push(i)
+function showNext() {
+
+    canShowPrev = true;
+
+    if(!canShowNext)
+        return;
+
+    startIndex = endIndex;
+    endIndex = endIndex + range;
+    print(startIndex, endIndex);
+
+    if((startIndex + range) > arr.length){
+        canShowNext = false;
+        console.log('end of array!:');
+    }
 }
+
+function showPrev() {
+    canShowNext = true;
+    if(!canShowPrev)
+        return;
+
+    endIndex = startIndex;
+    startIndex = endIndex - range;
+    print(startIndex, endIndex);
+
+    if((startIndex - range) < 0){
+        canShowPrev = false;
+        console.log('start of array!:');
+    }
+}
+
 console.log(arr);
-console.log('length', arr.length);
-console.log('arr[9]', arr[9]);
-var counter = 0;
-
-var startEl = 0;
-var endEl = 2;
-var step = endEl + 1;
-
-var allowNext = true;
-var allowPrev = true;
-
-function showNextElems() {
-    if(!allowNext)
-        return;
-    allowPrev = true;
-
-    printer(startEl + step, endEl + step);
-
-    if(((startEl + step) < arr.length - 1)) {
-        startEl = startEl + step;
-        // console.log('startEl',startEl);
-    } else {
-        startEl = startEl + step;
-        endEl = endEl + step;
-        allowNext = false;
-    }
-    if(((endEl + step) < arr.length - 1)) {
-        endEl = endEl + step;
-    } else {
-        console.log('its end of array!');
-
-    }
-}
-
-function showPrevlems() {
-    if(!allowPrev)
-        return;
-
-    allowNext = true;
-    printer(startEl - step, endEl - step);
-
-    if(((startEl - step) > 0)) {
-        startEl = startEl - step;
-    } else {
-        startEl = startEl - step;
-        endEl = endEl - step;
-        allowPrev = false;
-    }
-    if(((endEl - step) > 0) && (endEl - step) > step) {
-        endEl = endEl - step;
-    } else {
-        console.log('its start of array!');
-    }
-}
-
-printer(startEl, endEl);
-
-function printer(start, end) {
-    // console.log('startEl:' + start + ' endEl:' + end);
-
-    for(var n = start; n <= end; n++) {
-        // console.log('n', n);
-        if(arr[n] !== undefined) {
-            console.log(arr[n]);
-        }
-    }
-    console.log('________________________');
-
-}
-
+print(startIndex, endIndex);
